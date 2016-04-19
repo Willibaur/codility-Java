a511831b9eb7927e3deb07b931cb39cea73c37b0
William Bautista <williambautista@gmail.com>
2016-04-19 17:35:15 +0100

    Only digit phone numbers edge cases added

--- src/test/PhoneNumberFormatterTest.java
+++ src/test/PhoneNumberFormatterTest.java
@@ -20,16 +20,30 @@ public class PhoneNumberFormatterTest {
     }
 
     @Test
-    public void testRemoveNonDigits() {
+    public void testEvenPhoneNumbers() {
         input = "122!££31£$%-34%%%53 &&^&^45&*&^*()3-+=-{}[[[];:'@#~   555--";
         expected = "122-313-453-453-555";
         assertEquals("Removes all non digits", expected, phoneNumberFormatter.solution(input));
     }
 
     @Test
-    public void testCheckNumberSize() {
+    public void testOddPhoneNumbers() {
         input = "0 - 22 1985--342";
         expected = "022-198-53-42";
         assertEquals("Checks double dashes", expected, phoneNumberFormatter.solution(input));
+    }
+
+    @Test
+    public void testOddOnlyDigitPhoneNumbers() {
+        input = "1234567";
+        expected = "123-45-67";
+        assertEquals("Odd Only digit phone number", expected, phoneNumberFormatter.solution(input));
+    }
+
+    @Test
+    public void testEvenOnlyDigitPhoneNumbers() {
+        input = "1234567";
+        expected = "123-45-67";
+        assertEquals("Even Only digit phone number", expected, phoneNumberFormatter.solution(input));
     }
 }
